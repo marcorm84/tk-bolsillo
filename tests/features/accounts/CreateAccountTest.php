024802faf789ea661726a422d92342bdda31b72f
Junior Claudio Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-01-17 20:06:46 +0000

    test for create an account

---
+++ tests/features/accounts/CreateAccountTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CreateAccountTest extends TestCase
+{
+    /** @test */
+    public function user_can_register_an_account()
+    {
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user);
+
+        $this->visit('/accounts/add')
+             ->type('Bolsillo', 'name')
+             ->select('Soles', 'currency')
+             ->type('', 'description')
+             ->type('0.00', 'balance')
+             ->type('coins', 'type')
+             ->press('Add');
+
+        $this->seePageIs('/my-accounts')
+             ->see('Account added successfully.');
+    }
+}
