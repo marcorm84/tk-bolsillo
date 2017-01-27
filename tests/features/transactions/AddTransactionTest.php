1f1feb0bf324788470246fc0c37ee347421270bb
Junior Claudio <junior.claudio@tektonlabs.com>
2017-01-27 20:54:27 +0000

    test add transaction - add expense updated, dummy data added, try use json method instead of call

--- tests/features/transactions/AddTransactionTest.php
+++ tests/features/transactions/AddTransactionTest.php
@@ -6,37 +6,40 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class AddTransactionTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-
     use DatabaseMigrations;
-
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
 
     /**
     @test
     **/
-    public function user_can_add_income()
+    public function user_can_add_expenses()
     {
+        $user = factory(User::class)->create();
 
-        $accounts = $this->getAccounts();
+        $this->getAccounts();
 
-        $this->call('post', 'transactions', [
+        $this->be($user);
+
+        TransactionType::create(['name' => 'Income']);
+        TransactionType::create(['name' => 'Expenses']);
+
+        Category::create(['name' => 'Salary', 'transaction_type_id' => 1]);
+        Category::create(['name' => 'Savings', 'transaction_type_id' => 1]);
+
+        Category::create(['name' => 'Food', 'transaction_type_id' => 2]);
+        Category::create(['name' => 'Games', 'transaction_type_id' => 2]);
+
+        $this->json('post', 'transactions', [
             'account_id' => 1,
             'title' => 'pack empanadas + coca cola',
             'amount' => '12.50',
             'type' => 2,
-            'category' => 1,
+            'category' => 3,
             'labels' => ['empanadas', 'metro', 'tk'],
             'description' => 'empanadas en metro de tarata'
         ]);
 
         $this->assertResponseStatus(200);
+
+        $this->seeInDatabase('transactions', ['title' => 'pack empanadas + coca cola');
     }
 }
