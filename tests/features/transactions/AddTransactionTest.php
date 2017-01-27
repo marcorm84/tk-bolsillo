02e9e0e969b966b32e5d686d75e69ec30f130fc7
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2017-01-27 21:01:59 +0000

    migration and model for transaction type

--- tests/features/transactions/AddTransactionTest.php
+++ tests/features/transactions/AddTransactionTest.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use App\TransactionType;
+
 class AddTransactionTest extends TestCase
 {
     use DatabaseMigrations;
