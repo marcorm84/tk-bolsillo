b76111c6945cf941854f822d65c723bc9bcf3d19
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2017-01-30 20:21:52 +0000

    more test for create account test

--- tests/features/accounts/CreateAccountTest.php
+++ tests/features/accounts/CreateAccountTest.php
@@ -12,24 +12,107 @@ class CreateAccountTest extends TestCase
 {
     use DatabaseMigrations;
 
-    /** @test */
-    public function user_can_register_an_account()
+    function setUp()
     {
-        $user = factory(User::class)->create();
-        $currency_soles   = Currency::create(['name' => 'Soles']);
-        $currency_dollars = Currency::create(['name' => 'Dollars']);
+        parent::setUp();
+        Currency::create(['name' => 'Soles']);
+        Currency::create(['name' => 'Dollars']);
+    }
 
-        $this->actingAs($user);
+    /** @test */
+    function user_can_register_an_account()
+    {
+        $this->actingAs(factory(User::class)->create());
 
         $this->visit('/my-accounts/add')
              ->see('Soles')
              ->see('Dollars');
 
         $this->type('Pocket', 'name')
-             ->select($currency_soles->id, 'currency_id')
+             ->select(1, 'currency_id')
              ->type('', 'description')
              ->type('0.00', 'balance')
              ->press('Add');
+
+        $this->seePageIs('/my-accounts')
+             ->see('Account added successfully.');
+    }
+
+    /** @test */
+    function user_can_register_an_account_with_collaborators()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $this->post('/my-accounts/add', [
+            'name' => 'Pocket',
+            'currency_id' => 1,
+            'description' => 'bolsillo',
+            'balance' => 100.0,
+            'collaborators' => [
+                'test@example.com',
+                'prueba@prueba.com',
+                'admin@admin.com',
+                'larry@tekton.com'
+            ],
+        ]);
+
+        $this->followRedirects();
+
+        $this->seePageIs('/my-accounts')
+             ->see('Account added successfully.');
+
+        $this->seeInDatabase('account_collaborators', ['email' => 'test@example.com']);
+        $this->seeInDatabase('account_collaborators', ['email' => 'prueba@prueba.com']);
+        $this->seeInDatabase('account_collaborators', ['email' => 'admin@admin.com']);
+        $this->seeInDatabase('account_collaborators', ['email' => 'larry@tekton.com']);
+    }
+
+    /** @test */
+    function user_cannot_register_an_account_without_name()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $this->post('/my-accounts/add', [
+            'currency_id' => 1,
+            'description' => 'bolsillo',
+            'balance' => 100.0,
+        ]);
+
+        $this->followRedirects();
+
+        $this->seePageIs('/my-accounts/add')
+             ->see('The name field is required.');
+    }
+
+    /** @test */
+    function user_cannot_register_an_account_without_currency()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $this->post('/my-accounts/add', [
+            'name' => 'Pocket',
+            'description' => 'bolsillo',
+            'balance' => 100.0,
+        ]);
+
+        $this->followRedirects();
+
+        $this->seePageIs('/my-accounts/add')
+             ->see('The currency field is required.');
+    }
+
+    /** @test */
+    function user_can_register_an_account_without_description()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $this->post('/my-accounts/add', [
+            'name' => 'Pocket',
+            'currency_id' => 1,
+            'balance' => 100.0,
+        ]);
+
+        $this->followRedirects();
 
         $this->seePageIs('/my-accounts')
              ->see('Account added successfully.');
